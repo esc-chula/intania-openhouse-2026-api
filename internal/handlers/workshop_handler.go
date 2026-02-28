cc59102df0e520847c8c99ff93c85bf164bd2769
frostnzx <88520840+frostnzx@users.noreply.github.com>
2026-02-28 18:19:22 +0700

    fix: enable auth middleware in workshop handler

--- internal/handlers/workshop_handler.go
+++ internal/handlers/workshop_handler.go
@@ -31,7 +31,7 @@ func InitWorkshopHandler(api huma.API, usecase usecases.WorkshopUsecase, mid mid
 		mid:     mid,
 	}
 
-	// api.UseMiddleware(mid.WithAuthContext)
+	api.UseMiddleware(mid.WithAuthContext)
 
 	huma.Get(api, "/{id}", handler.GetWorkshop, func(o *huma.Operation) {
 		o.Summary = "Get workshop details"
