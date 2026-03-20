a533886d6bd92ee843f5f4a7c494e78512f55acd
DeepAung <i.deepaung@gmail.com>
2026-03-20 09:14:47 +0700

    feat: auto migrate-up when run serve command

--- cmd/cmd_serve.go
+++ cmd/cmd_serve.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"context"
 	"log"
+	"time"
 
+	"github.com/esc-chula/intania-openhouse-2026-api/internal/migrations"
 	"github.com/esc-chula/intania-openhouse-2026-api/internal/server"
+	"github.com/esc-chula/intania-openhouse-2026-api/pkg/database"
+	"github.com/pressly/goose/v3"
 	"github.com/spf13/cobra"
 )
 
@@ -16,8 +21,28 @@ var serveCmd = &cobra.Command{
 			return err
 		}
 
+		db := database.NewPostgresDB(cfg.Database())
+
+		// migrate up here
+		err = func() error {
+			ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+			defer cancel()
+
+			goose.SetBaseFS(migrations.Migrations)
+			if err := goose.SetDialect("postgres"); err != nil {
+				return err
+			}
+			if err := goose.UpContext(ctx, db.DB, "."); err != nil {
+				return err
+			}
+			return nil
+		}()
+		if err != nil {
+			return err
+		}
+
 		log.Printf("Listening on address %s", cfg.App().Address)
 
-		return server.InitServer(cfg)
+		return server.InitServer(cfg, db)
 	},
 }
