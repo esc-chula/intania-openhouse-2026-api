660f33248ae7bb0ca44354a03577584f8cf5ae06
DeepAung <i.deepaung@gmail.com>
2026-03-26 21:50:58 +0700

    fix: remove origin_location_enum in database

---
+++ internal/migrations/20260326144405_update_origin_location_enum.sql
@@ -0,0 +1,140 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE users ALTER COLUMN origin_location TYPE VARCHAR(255) USING origin_location::text;
+
+DROP TYPE IF EXISTS origin_location_enum;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+CREATE TYPE origin_location_enum AS ENUM (
+    -- เขตกรุงเทพ
+    'phra_nakhon', -- เขตพระนคร
+    'dusit', -- เขตดุสิต
+    'nong_chok', -- เขตหนองจอก
+    'bang_rak', -- เขตบางรัก
+    'bang_khen', -- เขตบางเขน
+    'bang_kapi', -- เขตบางกะปิ
+    'pathum_wan', -- เขตปทุมวัน
+    'pom_prap_sattru_phai', -- เขตป้อมปราบศัตรูพ่าย
+    'phra_khanong', -- เขตพระโขนง
+    'min_buri', -- เขตมีนบุรี
+    'lat_krabang', -- เขตลาดกระบัง
+    'yan_nawa', -- เขตยานนาวา
+    'khlong_san', -- เขตคลองสาน
+    'bang_khae', -- เขตบางแค
+    'bang_kho_laem', -- เขตบางคอแหลม
+    'bang_sue', -- เขตบางซื่อ
+    'bang_na', -- เขตบางนา
+    'thawi_watthana', -- เขตทวีวัฒนา
+    'thung_khru', -- เขตทุ่งครุ
+    'bang_plad', -- เขตบางพลัด
+    'bang_bon', -- เขตบางบอน
+    'bang_khun_thian', -- เขตบางขุนเทียน
+    'phasi_charoen', -- เขตภาษีเจริญ
+    'taling_chan', -- เขตตลิ่งชัน
+    'chatuchak', -- เขตจตุจักร
+    'lak_si', -- เขตหลักสี่
+    'sai_mai', -- เขตสายไหม
+    'khlong_toei', -- เขตคลองเตย
+    'suan_luang', -- เขตสวนหลวง
+    'rat_burana', -- เขตราษฎร์บูรณะ
+    'huai_khwang', -- เขตห้วยขวาง
+    'khlong_sam_wa', -- เขตคลองสามวา
+    'wang_thonglang', -- เขตวังทองหลาง
+    'saphan_sung', -- เขตสะพานสูง
+    'bangkok_yai', -- เขตบางกอกใหญ่
+    'bangkok_noi', -- เขตบางกอกน้อย
+    'samphanthawong', -- เขตสัมพันธวงศ์
+    'phaya_thai', -- เขตพญาไท
+    'ratchathewi', -- เขตราชเทวี
+    'don_mueang', -- เขตดอนเมือง
+    'prawet', -- เขตประเวศ
+    'din_daeng', -- เขตดินแดง
+    'bueng_kum', -- เขตบึงกุ่ม
+    'sathon', -- เขตสาทร
+    'chom_thong', -- เขตจอมทอง
+    'watthana', -- เขตวัฒนา
+    'kannayao', -- เขตคันนายาว
+
+    -- จังหวัด
+    'amnat_charoen', -- อำนาจเจริญ
+    'ang_thong', -- อ่างทอง
+    'bueng_kan', -- บึงกาฬ
+    'buriram', -- บุรีรัมย์
+    'chachoengsao', -- ฉะเชิงเทรา
+    'chai_nat', -- ชัยนาท
+    'chaiyaphum', -- ชัยภูมิ
+    'chanthaburi', -- จันทบุรี
+    'chiang_mai', -- เชียงใหม่
+    'chiang_rai', -- เชียงราย
+    'chonburi', -- ชลบุรี
+    'chumphon', -- ชุมพร
+    'kalasin', -- กาฬสินธุ์
+    'kamphaeng_phet', -- กำแพงเพชร
+    'kanchanaburi', -- กาญจนบุรี
+    'khon_kaen', -- ขอนแก่น
+    'krabi', -- กระบี่
+    'lampang', -- ลำปาง
+    'lamphun', -- ลำพูน
+    'loei', -- เลย
+    'lopburi', -- ลพบุรี
+    'mae_hong_son', -- แม่ฮ่องสอน
+    'maha_sarakham', -- มหาสารคาม
+    'mukdahan', -- มุกดาหาร
+    'nakhon_nayok', -- นครนายก
+    'nakhon_pathom', -- นครปฐม
+    'nakhon_phanom', -- นครพนม
+    'nakhon_ratchasima', -- นครราชสีมา
+    'nakhon_sawan', -- นครสวรรค์
+    'nakhon_si_thammarat', -- นครศรีธรรมราช
+    'nan', -- น่าน
+    'narathiwat', -- นราธิวาส
+    'nong_bua_lamphu', -- หนองบัวลำภู
+    'nong_khai', -- หนองคาย
+    'nonthaburi', -- นนทบุรี
+    'pathum_thani', -- ปทุมธานี
+    'pattani', -- ปัตตานี
+    'phang_nga', -- พังงา
+    'phatthalung', -- พัทลุง
+    'phayao', -- พะเยา
+    'phetchabun', -- เพชรบูรณ์
+    'phetchaburi', -- เพชรบุรี
+    'phichit', -- พิจิตร
+    'phitsanulok', -- พิษณุโลก
+    'phra_nakhon_si_ayutthaya', -- พระนครศรีอยุธยา
+    'phrae', -- แพร่
+    'phuket', -- ภูเก็ต
+    'prachinburi', -- ปราจีนบุรี
+    'prachuap_khiri_khan', -- ประจวบคีรีขันธ์
+    'ranong', -- ระนอง
+    'ratchaburi', -- ราชบุรี
+    'rayong', -- ระยอง
+    'roi_et', -- ร้อยเอ็ด
+    'sa_kaeo', -- สระแก้ว
+    'sakon_nakhon', -- สกลนคร
+    'samut_prakan', -- สมุทรปราการ
+    'samut_sakhon', -- สมุทรสาคร
+    'samut_songkhram', -- สมุทรสงคราม
+    'sara_buri', -- สระบุรี
+    'satun', -- สตูล
+    'sing_buri', -- สิงห์บุรี
+    'sisaket', -- ศรีสะเกษ
+    'songkhla', -- สงขลา
+    'sukhothai', -- สุโขทัย
+    'suphan_buri', -- สุพรรณบุรี
+    'surat_thani', -- สุราษฎร์ธานี
+    'surin', -- สุรินทร์
+    'tak', -- ตาก
+    'trang', -- ตรัง
+    'trat', -- ตราด
+    'ubon_ratchathani', -- อุบลราชธานี
+    'udon_thani', -- อุดรธานี
+    'uthai_thani', -- อุทัยธานี
+    'uttaradit', -- อุตรดิตถ์
+    'yala', -- ยะลา
+    'yasothon' -- ยโสธร
+);
+
+ALTER TABLE users ALTER COLUMN origin_location TYPE origin_location_enum USING origin_location::origin_location_enum;
+-- +goose StatementEnd
